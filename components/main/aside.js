5fdb46a8c0c54e555d01fa794b23a470b2c8d225
오종택 <jongtaek@ojongtaeg-ui-MacBookPro.local>
2021-12-15 03:20:15 +0900

    add: aside 레이아웃 wip

--- components/main/aside.js
+++ components/main/aside.js
@@ -1,7 +1,15 @@
 export default async function Aside() {
   return `
     <aside class="aside" style="left: 0;">
-      <div>aside</div>
+      <div class="aside_profile">
+        <img class="aside_profile_image" alt="aside_profile_image" src="https://ca.slack-edge.com/T0F25KY9Y-U020D7262KH-3ac395a4150d-512" />
+        <div class="aside_profile_info">
+          <p class="bold">saengmotmi</p>
+          <p class="text-gray bold">오종</p>
+        </div>
+        <span class="account_transition text-blue">전환</span>
+      </div>
+      <div>회원님을 위한 추천</div>
     </aside>
   `;
 }
